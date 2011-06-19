834a1d94ba386f3ed9e99f549208a1a63db2bd7c
Alex Gaynor <alex.gaynor@gmail.com>
2011-06-19 10:23:44 -0700

    Now handles basic jumping.

--- decompile.py
+++ decompile.py
@@ -1,15 +1,34 @@
 import opcode
 
 
+NO_ARG = object()
 class Instruction(object):
-    def __init__(self, opname, arg=None):
-        self.opname = opname
+    def __init__(self, op, arg=NO_ARG, real_idx=None, new_idx=None):
+        self.op = op
+        self.opname = opcode.opname[op]
         self.arg = arg
+        self.real_idx = real_idx
+        self.new_idx = new_idx
+
+    def __repr__(self):
+        r = self.opname
+        if self.arg is not NO_ARG:
+            r += "(%s)" % self.arg
+        return r
 
     def __eq__(self, other):
         if not isinstance(other, Instruction):
             return NotImplemented
         return self.opname == other.opname and self.arg == other.arg
+
+    def finish(self, instructions):
+        if self.op in opcode.hasjabs:
+            for idx, instr in enumerate(instructions):
+                if instr.real_idx == self.arg:
+                    self.arg = idx
+                    break
+            else:
+                assert False
 
 def parse_bytecode(code):
     i = 0
@@ -17,19 +36,28 @@ def parse_bytecode(code):
     instructions = []
     while i < len(bytecodes):
         op = bytecodes[i]
+        opidx = i
         i += 1
-        arg = None
+        arg = NO_ARG
         if op >= opcode.HAVE_ARGUMENT:
             oparg = bytecodes[i] + (bytecodes[i + 1] << 8)
             i += 2
             if op in opcode.hasconst:
                 arg = code.co_consts[oparg]
+            elif op in opcode.hasname:
+                arg = code.co_names[oparg]
+            elif op in opcode.hasjabs:
+                arg = oparg
             else:
                 raise NotImplementedError
         instructions.append(Instruction(
-            opcode.opname[op],
-            arg
+            op,
+            arg,
+            opidx,
+            len(instructions),
         ))
+    for instr in instructions:
+        instr.finish(instructions)
     return instructions
 
 class Literal(object):
@@ -40,11 +68,13 @@ class Literal(object):
         return str(self.obj)
 
 class Interpreter(object):
-    def __init__(self, instructions):
+    def __init__(self, instructions, start_instr, stop_instr, indent_level=1):
         self.instructions = instructions
         self.ops = []
         self.buf = []
-        self.indent_level = 1
+        self.current_instr = start_instr
+        self.stop_instr = stop_instr
+        self.indent_level = indent_level
 
     def get_and_clear_buf(self, expected_len):
         assert len(self.buf) == expected_len
@@ -53,24 +83,53 @@ class Interpreter(object):
         return buf
 
     def emit(self, op):
-        self.ops.append("    " * self.indent_level + op)
+        if isinstance(op, Interpreter):
+            self.ops.append(op.evaluate())
+        else:
+            self.ops.append("    " * self.indent_level + op)
 
     def evaluate(self):
-        for instr in self.instructions:
+        while self.current_instr < self.stop_instr:
+            current_instr = self.current_instr
+            instr = self.instructions[current_instr]
             handler = getattr(self, "handle_%s" % instr.opname)
             handler(instr)
+            # The current instruction was unchanged by the handler, we need to
+            # bump it.
+            if self.current_instr == current_instr:
+                self.current_instr += 1
         return "\n".join(self.ops)
 
     def handle_LOAD_CONST(self, instr):
+        self.buf.append(Literal(instr.arg))
+
+    def handle_LOAD_GLOBAL(self, instr):
         self.buf.append(Literal(instr.arg))
 
     def handle_RETURN_VALUE(self, instr):
         [obj] = self.get_and_clear_buf(1)
         self.emit("return %s" % obj.build())
 
+    def handle_POP_JUMP_IF_FALSE(self, instr):
+        [obj] = self.get_and_clear_buf(1)
+        self.emit("if %s:" % obj.build())
+        self.emit(Interpreter(
+            self.instructions,
+            instr.new_idx + 1,
+            instr.arg,
+            self.indent_level + 1
+        ))
+        self.emit("else:")
+        self.emit(Interpreter(
+            self.instructions,
+            instr.arg,
+            len(self.instructions),
+            self.indent_level + 1,
+        ))
+        self.current_instr = len(self.instructions)
 
 def decompile(function):
     instructions = parse_bytecode(function.__code__)
-    body = Interpreter(instructions).evaluate()
+    body = Interpreter(instructions, 0, len(instructions)).evaluate()
     header = "def %s():\n" % function.__name__
     return header + body
--- test_decompile.py
+++ test_decompile.py
@@ -1,3 +1,4 @@
+import opcode
 import textwrap
 
 from decompile import decompile, parse_bytecode, Instruction
@@ -18,15 +19,64 @@ class TestDecompilation(object):
                 return 1
         """)
 
+    def test_branch(self):
+        def f():
+            if z:
+                return 1
+            else:
+                return 2
+
+        # This needs some dead code eliminiation applied to it.
+        self.assert_decompiles(f, """
+            def f():
+                if z:
+                    return 1
+                else:
+                    return 2
+                    return None
+        """)
+
 class TestBytecode(object):
-    def assert_bytecodes(self, func, expected):
-        assert parse_bytecode(func.__code__) == expected
+    def assert_bytecode(self, func, expected):
+        instructions = parse_bytecode(func.__code__)
+        expected = [
+            Instruction(opcode.opmap[args[0]], *args[1:])
+            for args in expected
+        ]
+        assert instructions == expected
 
     def test_simple(self):
         def f():
             return 1
 
-        self.assert_bytecodes(f, [
-            Instruction("LOAD_CONST", 1),
-            Instruction("RETURN_VALUE",)
+        self.assert_bytecode(f, [
+            ("LOAD_CONST", 1),
+            ("RETURN_VALUE",),
+        ])
+
+    def test_load_global(self):
+        def f():
+            return z
+
+        self.assert_bytecode(f, [
+            ("LOAD_GLOBAL", "z"),
+            ("RETURN_VALUE",),
+        ])
+
+    def test_simple_branch(self):
+        def f():
+            if z:
+                return 1
+            else:
+                return 2
+
+        self.assert_bytecode(f, [
+            ("LOAD_GLOBAL", "z"),
+            ("POP_JUMP_IF_FALSE", 4),
+            ("LOAD_CONST", 1),
+            ("RETURN_VALUE",),
+            ("LOAD_CONST", 2),
+            ("RETURN_VALUE",),
+            ("LOAD_CONST", None),
+            ("RETURN_VALUE",)
         ])
